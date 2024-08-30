90c27a97619064273c615ea6ac1cb3f9d6905c17
Peter Kulko <peter.kulko@raccoongang.com>
2024-08-30 09:13:38 +0300

    feat: added updates for hw 3

--- hw_3_flask_views/main.py
+++ hw_3_flask_views/main.py
@@ -2,17 +2,35 @@ import os
 import httpx
 from http import HTTPStatus
 
-from flask import Flask, render_template, abort, send_file, Response
+from flask import Flask, jsonify, render_template, abort, send_file, Response
 from faker import Faker
 from webargs.flaskparser import use_kwargs
 import pandas as pd
 
+from database_handler import execute_query
 from settings import CSV_FILE_PATH, BASE_CURRENCY_URL
 from validators import number_of_users_config, bitcoin_search_config
 from utils import generate_user_data, fetch_data, filter_by_code, get_currency_symbol
 
-
 app = Flask(__name__)
+
+
+@app.errorhandler(HTTPStatus.NOT_FOUND)
+@app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY)
+@app.errorhandler(HTTPStatus.BAD_REQUEST)
+def error_handling(error):
+    print('error =========', error)
+    data = getattr(error, 'data', {})
+    headers = data.get('headers', None)
+    messages = data.get('messages', ['Invalid request.'])
+
+    response = jsonify({'errors': messages})
+    response.status_code = error.code
+
+    if headers:
+        response.headers.extend(headers)
+
+    return response
 
 
 @app.route('/generate-students')
@@ -24,13 +42,13 @@ def generate_students(number_of_users):
     """
     faker_instance = Faker()
     users = generate_user_data(faker_instance, number_of_users)
-    
+
     try:
         data_frame = pd.DataFrame(users)
         data_frame.to_csv(CSV_FILE_PATH, index=False)
     except Exception as e:
         abort(500, description=f'Error saving CSV: {e}')
-    
+
     return render_template('users.html', users=users)
 
 
@@ -52,27 +70,34 @@ def get_bitcoin_value(currency, amount_of_currency):
     try:
         rates_data = fetch_data(f'{BASE_CURRENCY_URL}/api/rates')
         currencies_data = fetch_data(f'{BASE_CURRENCY_URL}/currencies').get('data', [])
-        
+
         filtered_currency_data = filter_by_code(rates_data, currency)
         if not filtered_currency_data:
             return Response(f'ERROR: Currency {currency} not found.', status=HTTPStatus.BAD_REQUEST)
-        
+
         total_amount_of_currency = amount_of_currency * filtered_currency_data['rate']
         currency_symbol = get_currency_symbol(currencies_data, currency)
-        
+
         context = {
             'filtered_currency_date': filtered_currency_data,
             'total_amount_of_currency': total_amount_of_currency,
             'currency_symbol': currency_symbol
         }
         return render_template('crypto_currency.html', **context)
-    
+
     except httpx.HTTPStatusError as e:
         return Response(f'ERROR: Failed to fetch data from the API. {str(e)}', status=HTTPStatus.BAD_REQUEST)
     except TypeError as e:
         return Response(f'ERROR: Data format issue. {str(e)}', status=HTTPStatus.INTERNAL_SERVER_ERROR)
     except Exception as e:
         return Response(f'ERROR: An unexpected error occurred. {str(e)}', status=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@app.route('/customers')
+def get_all_customers():
+    query = "SELECT * FROM customers"
+    data = execute_query(query=query)
+    return data
 
 
 if __name__ == '__main__':
